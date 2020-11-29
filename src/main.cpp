549b3c8ad4821d19662df2febc49cb2607ff26da
Guido Westenberg <gwestenberg@pivotal.io>
2020-11-28 20:54:40 -0600

    Separate patterns for controllers

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,12 @@ extern void dots(CRGB color, int interval, int offset = 0);
 CRGB black     = CRGB(0x00000000);
 CRGB darkgrey  = CRGB(0x00202020);
 CRGB halloween = CRGB(0x00FF3F00);
+CRGB red       = CRGB(0x00DD0000);
 CRGB darkred   = CRGB(0x00C00000);
 CRGB purple    = CRGB(0x00400090);
 CRGB limegreen = CRGB(0x0080FF00);
-CRGB warm      = CRGB(0x00FF9922);
+CRGB warm      = CRGB(0x00DD7722);
+CRGB office    = CRGB(0x00DD6611);
 
 /**************************************************************************************/
 /* Main                                                                               */
@@ -36,7 +38,12 @@ void setup() {
   // dots(halloween, 4);
   // dots(darkred, 8);
   // dots(purple, 8, 4);
-  dots(warm, 4);
+#ifdef MEGA
+  dots(warm, 8);
+  dots(red, 8, 4);
+#else
+  solid(office);
+#endif
   powerOn();
 }
 
